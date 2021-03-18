396fcc0e4a1db6fa62f6b7fe675ac8765410e36d
funkyfisch <funkyfisch@gmail.com>
2021-03-18 18:24:47 +0100

    Add code coverage

--- src/index.js
+++ src/index.js
@@ -12,4 +12,27 @@ const dayNumberToDayName = function (number) {
   return dayMap[number]
 }
 
-module.exports = dayNumberToDayName
+const determineSeniority = function (yearsOfExperience) {
+  if (
+    typeof yearsOfExperience !== 'number'
+    || yearsOfExperience < 0
+  ) throw new Error("parameter must be a positive number.")
+
+  switch (true) {
+    case (yearsOfExperience === 0):
+      return "student"
+    case (yearsOfExperience > 0 && yearsOfExperience <= 3):
+      return "junior"
+    case (yearsOfExperience > 3 && yearsOfExperience <= 5):
+      return "mid-level"
+    case (yearsOfExperience > 5 && yearsOfExperience <= 10):
+      return "senior"
+    case (yearsOfExperience > 10):
+      return "expert"
+  }
+}
+
+module.exports = {
+  dayNumberToDayName,
+  determineSeniority,
+}
