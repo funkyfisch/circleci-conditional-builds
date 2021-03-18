7f1e18dd9633c6bf3292763b1d9801f6084e4e38
Ioannis Gkikas <funkyfisch@gmail.com>
2021-03-18 18:46:23 +0100

    Add code coverage (#6)
    * Add code coverage
    
    * Upload coverage report
    
    * Enable code coverage report on merge

--- test/unit/index.spec.js
+++ test/unit/index.spec.js
@@ -1,6 +1,11 @@
-const dayNumberToDayName = require('../../src/index')
+const { dayNumberToDayName, determineSeniority } = require('../../src/index')
 
 test("First day of week is Monday", () => {
   expect(dayNumberToDayName(1)).toBe("Monday")
+
+})
+
+test("Person with 7 years of experience is considered senior", () => {
+  expect(determineSeniority(7)).toBe("senior")
 
 })
